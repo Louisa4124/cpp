5cc8889dc107823c187017c194debb36d45a4e01
Louiza Boudjemai <lboudjem@z2r3p3.42lyon.fr>
2023-08-23 15:11:43 +0200

    add : ex02/cpp1

---
+++ cpp1/ex03/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lboudjem <lboudjem@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 15:09:45 by lboudjem          #+#    #+#             */
+/*   Updated: 2023/08/23 15:11:15 by lboudjem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+	{
+		// Weapon club = Weapon("crude spiked club");
+		// HumanA bob("Bob", club);
+		// bob.attack();
+		// club.setType("some other type of club");
+		// bob.attack();
+	}
+	{
+		// Weapon club = Weapon("crude spiked club");
+		// HumanB jim("Jim");
+		// jim.setWeapon(club);
+		// jim.attack();
+		// club.setType("some other type of club");
+		// jim.attack();
+	}
+	return 0;
+}
